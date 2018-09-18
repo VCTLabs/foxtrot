680850934081f269d83be50c5c2a0aa16223c86f
S. Lockwood-Childs <sjl@vctlabs.com>
2018-09-17 17:05:26 -0700

    implement fuse methods that modify the share
    Added following methods:
    * mkdir
    * unlink
    * rmdir
    * rename
    * truncate
    * write
    * create
    * ftruncate
    
    Changed 'open' method so that it allows opening files in writable mode

--- foxtrot.c
+++ foxtrot.c
@@ -96,6 +96,168 @@ static int foxtrot_getattr(const char *path, struct stat *stbuf)
     return result;
 }
 
+static int foxtrot_mkdir(const char *path, mode_t mode)
+{
+    char *smbpath = NULL;
+    int result;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "mkdir path=%s mode=%4o\n", path, mode);
+#endif
+
+    if (path[0] != '/')
+    {
+        WARN("Relative path ignored!");
+        return -ENOENT;
+    }
+
+    smbpath = mksmbpath(path);
+    if (smbpath == NULL)
+    {
+        WARN("Path too long");
+        return -ENOENT;
+    }
+
+    pthread_mutex_lock(&global_mutex);
+    if (smbc_mkdir(smbpath, mode) == 0) result = 0; else result = -errno;
+    free(smbpath);
+    pthread_mutex_unlock(&global_mutex);
+
+    return result;
+}
+
+static int foxtrot_unlink(const char *path)
+{
+    char *smbpath = NULL;
+    int result;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "unlink path=%s\n", path);
+#endif
+
+    if (path[0] != '/')
+    {
+        WARN("Relative path ignored!");
+        return -ENOENT;
+    }
+
+    smbpath = mksmbpath(path);
+    if (smbpath == NULL)
+    {
+        WARN("Path too long");
+        return -ENOENT;
+    }
+
+    pthread_mutex_lock(&global_mutex);
+    if (smbc_unlink(smbpath) == 0) result = 0; else result = -errno;
+    free(smbpath);
+    pthread_mutex_unlock(&global_mutex);
+
+    return result;
+}
+
+static int foxtrot_rmdir(const char *path)
+{
+    char *smbpath = NULL;
+    int result;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "rmdir path=%s\n", path);
+#endif
+
+    if (path[0] != '/')
+    {
+        WARN("Relative path ignored!");
+        return -ENOENT;
+    }
+
+    smbpath = mksmbpath(path);
+    if (smbpath == NULL)
+    {
+        WARN("Path too long");
+        return -ENOENT;
+    }
+
+    pthread_mutex_lock(&global_mutex);
+    if (smbc_rmdir(smbpath) == 0) result = 0; else result = -errno;
+    free(smbpath);
+    pthread_mutex_unlock(&global_mutex);
+
+    return result;
+}
+
+static int foxtrot_rename(const char *from, const char *to)
+{
+    char *smbfrom = NULL, *smbto = NULL;
+    int result;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "rename from=%s to=%s\n", from, to);
+#endif
+
+    if (from[0] != '/' || to[0] != '/')
+    {
+        WARN("Relative path ignored!");
+        return -ENOENT;
+    }
+
+    smbfrom = mksmbpath(from);
+    smbto = mksmbpath(to);
+    if (smbfrom == NULL || smbto == NULL)
+    {
+        WARN("Path too long");
+        return -ENOENT;
+    }
+
+    pthread_mutex_lock(&global_mutex);
+    if (smbc_rename(smbfrom, smbto) == 0) result = 0; else result = -errno;
+    free(smbfrom);
+    free(smbto);
+    pthread_mutex_unlock(&global_mutex);
+
+    return result;
+}
+
+static int foxtrot_truncate(const char *path, off_t size)
+{
+    char *smbpath = NULL;
+    int fd;
+    int result;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "truncate path=%s %ld\n", path, size);
+#endif
+
+    if (path[0] != '/')
+    {
+        WARN("Relative path ignored!");
+        return -ENOENT;
+    }
+
+    smbpath = mksmbpath(path);
+    if (smbpath == NULL)
+    {
+        WARN("Path too long");
+        return -ENOENT;
+    }
+
+    pthread_mutex_lock(&global_mutex);
+    fd = smbc_open(smbpath, O_WRONLY, 0);
+    free(smbpath);
+    if (fd >= 0)
+    {
+        if (smbc_ftruncate(fd, size) == 0) result = 0; else result = -errno;
+        smbc_close(fd);
+    }
+    else
+    {
+        result = -errno;
+    }
+    pthread_mutex_unlock(&global_mutex);
+
+    return result;
+}
+
 static int foxtrot_readdir( const char *path, void *buf, fuse_fill_dir_t filler,
                            off_t offset, struct fuse_file_info *fi )
 {
@@ -149,12 +311,8 @@ static int foxtrot_open(const char *path, struct fuse_file_info *fi)
     int fd;
 
 #if TRACE_INVOCATIONS
-    fprintf(stderr, "open path=%s flags=%d\n", path, (int)fi->flags);
+    fprintf(stderr, "open path=%s flags=0%o\n", path, (int)fi->flags);
 #endif
-
-    /* Enforce read-only access for now, since write is not implemented */
-    if((fi->flags & 3) != O_RDONLY)
-        return -EACCES;
 
     smbpath = mksmbpath(path);
     if (smbpath == NULL)
@@ -164,7 +322,7 @@ static int foxtrot_open(const char *path, struct fuse_file_info *fi)
     }
 
     pthread_mutex_lock(&global_mutex);
-    fd = smbc_open(smbpath, O_RDONLY, 0644);
+    fd = smbc_open(smbpath, fi->flags & 3, 0644);
     free(smbpath);
     pthread_mutex_unlock(&global_mutex);
     if (fd < 0)
@@ -216,6 +374,47 @@ int foxtrot_read(const char *path, char *buf, size_t size, off_t offset,
     pthread_mutex_unlock(&global_mutex);
 
     return (int)nread;
+}
+
+static int foxtrot_write(const char *path, const char *buf, size_t size,
+       off_t offset, struct fuse_file_info *fi)
+{
+    size_t nwritten, chunk;
+    ssize_t result;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "read path=%s fd=%d size=%lld offset=%lld\n",
+                    path, (int)fi->fh, (long long)size, (long long)offset);
+#else
+    (void)path;
+#endif
+
+    pthread_mutex_lock(&global_mutex);
+    if (smbc_lseek((int)fi->fh, offset, SEEK_SET) == (off_t)-1)
+    {
+        WARN("smbc_lseek failed");
+        pthread_mutex_unlock(&global_mutex);
+        return -errno;
+    }
+
+    nwritten = 0;
+    while (nwritten < size)
+    {
+        chunk = size - nwritten;
+        if (chunk > SMB_MAX_PACKET_LENGTH) chunk = SMB_MAX_PACKET_LENGTH;
+        result = smbc_write((int)fi->fh, buf + nwritten, chunk);
+        if (result < 0)
+        {
+            WARN("smbc_write failed");
+            pthread_mutex_unlock(&global_mutex);
+            return -errno;
+        }
+        if (result == 0) break; /* EOF */
+        nwritten += result;
+    }
+    pthread_mutex_unlock(&global_mutex);
+
+    return (int)nwritten;
 }
 
 int foxtrot_release(const char *path, struct fuse_file_info *fi)
@@ -289,6 +488,53 @@ void foxtrot_destroy(void *private_data)
     pthread_mutex_destroy(&global_mutex);
 }
 
+static int foxtrot_create(const char *path, mode_t mode, struct fuse_file_info *fi)
+{
+    char *smbpath = NULL;
+    int fd;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "create path=%s mode=%4o flags=0%o\n", path, mode, (int)fi->flags);
+#endif
+
+    smbpath = mksmbpath(path);
+    if (smbpath == NULL)
+    {
+        WARN("Path too long");
+        return -ENOENT;
+    }
+
+    pthread_mutex_lock(&global_mutex);
+    fd = smbc_creat(smbpath, mode);
+    free(smbpath);
+    pthread_mutex_unlock(&global_mutex);
+    if (fd < 0)
+    {
+        WARN("smbc_creat failed");
+        return -fd;
+    }
+    fi->fh = fd;
+
+    return 0;
+}
+
+static int foxtrot_ftruncate(const char *path, off_t size, struct fuse_file_info *fi)
+{
+    int result;
+
+#if TRACE_INVOCATIONS
+    fprintf(stderr, "ftruncate path=%s fd=%d size=%ld\n", path, (int)fi->fh, size);
+#else
+    (void)path;
+#endif
+
+    pthread_mutex_lock(&global_mutex);
+    if (smbc_ftruncate(fi->fh, size) == 0) result = 0; else result = -errno;
+    pthread_mutex_unlock(&global_mutex);
+
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
     struct fuse_operations ops;
@@ -340,12 +586,20 @@ int main(int argc, char *argv[])
     /* Assign FUSE operations */
     memset(&ops, 0, sizeof(ops));
     ops.getattr = foxtrot_getattr;
+    ops.mkdir   = foxtrot_mkdir;
+    ops.unlink  = foxtrot_unlink;
+    ops.rmdir   = foxtrot_rmdir;
+    ops.rename  = foxtrot_rename;
+    ops.truncate = foxtrot_truncate;
     ops.readdir = foxtrot_readdir;
     ops.open    = foxtrot_open;
     ops.read    = foxtrot_read;
+    ops.write   = foxtrot_write;
     ops.release = foxtrot_release;
     ops.init    = foxtrot_init;
     ops.destroy = foxtrot_destroy;
+    ops.create  = foxtrot_create;
+    ops.ftruncate = foxtrot_ftruncate;
 
     return fuse_main(next_fuse_opt_idx, fuse_argv, &ops, NULL);
 }
